8210739ed50e085e04b4eae81a161b4c3cde1db7
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2021-08-10 22:06:29 +0200

    go mod vendor

--- vendor/github.com/rancher/k3s/pkg/daemons/config/types.go
+++ vendor/github.com/rancher/k3s/pkg/daemons/config/types.go
@@ -36,7 +36,7 @@ type Node struct {
 	Containerd               Containerd
 	Images                   string
 	AgentConfig              Agent
-	CACerts                  []byte
+	Token                    string
 	Certificate              *tls.Certificate
 	ServerHTTPSPort          int
 }
@@ -96,7 +96,6 @@ type Agent struct {
 	AirgapExtraRegistry     []string
 	DisableCCM              bool
 	DisableNPC              bool
-	DisableKubeProxy        bool
 	Rootless                bool
 	ProtectKernelDefaults   bool
 }
@@ -258,10 +257,13 @@ func (a ArgString) String() string {
 	return b.String()
 }
 
-func GetArgsList(argsMap map[string]string, extraArgs []string) []string {
+// GetArgs appends extra arguments to existing arguments overriding any default options.
+func GetArgs(argsMap map[string]string, extraArgs []string) []string {
+	const hyphens = "--"
+
 	// add extra args to args map to override any default option
 	for _, arg := range extraArgs {
-		splitArg := strings.SplitN(arg, "=", 2)
+		splitArg := strings.SplitN(strings.TrimPrefix(arg, hyphens), "=", 2)
 		if len(splitArg) < 2 {
 			argsMap[splitArg[0]] = "true"
 			continue
@@ -270,7 +272,7 @@ func GetArgsList(argsMap map[string]string, extraArgs []string) []string {
 	}
 	var args []string
 	for arg, value := range argsMap {
-		cmd := fmt.Sprintf("--%s=%s", arg, value)
+		cmd := fmt.Sprintf("%s%s=%s", hyphens, strings.TrimPrefix(arg, hyphens), value)
 		args = append(args, cmd)
 	}
 	sort.Strings(args)
